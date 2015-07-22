63d1b28282f2169c88cb18cf76f1779c7027f258
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2015-07-21 21:36:17 -0400

    add test that starts/stops replicaset

--- test/index.test.js
+++ test/index.test.js
@@ -18,4 +18,23 @@ describe('run', function() {
       });
     });
   });
+
+  it('should start a replicaset', function(done) {
+    var opts = {
+      action: 'start',
+      name: 'mongodb-runner-test-replicaset',
+      topology: 'replicaset',
+      port: 27002
+    };
+
+    run(opts, function(err) {
+      if (err) return done(err);
+
+      opts.action = 'stop';
+      run(opts, function(err) {
+        if (err) return done(err);
+        done();
+      });
+    });
+  });
 });
