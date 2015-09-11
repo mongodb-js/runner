4050efef02f6e060218d82621e18dec8ab6100e8
Judah Schvimer <judah.schvimer@mongodb.com>
2015-09-11 11:52:58 -0400

    Standalone cr auth works

--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,16 @@
 var run = require('../');
 var kill = require('kill-mongodb');
+var assert = require('assert');
+var mongodb = require('mongodb');
+var debug = require('debug')('mongodb-runner:index.test');
+var format = require('util').format;
+
 
 describe('run', function() {
   before(function(done) {
     kill(done);
   });
+
   it('should start a standalone', function(done) {
     var opts = {
       action: 'start',
@@ -14,7 +20,6 @@ describe('run', function() {
 
     run(opts, function(err) {
       if (err) return done(err);
-
       opts.action = 'stop';
       run(opts, function(err) {
         if (err) return done(err);
@@ -59,4 +64,79 @@ describe('run', function() {
       });
     });
   });
+
+  it('should start a standalone with auth', function(done) {
+    var opts = {
+      action: 'start',
+      name: 'mongodb-runner-test-standalone-auth',
+      port: 30000,
+      auth: true
+    };
+
+    run(opts, function(err) {
+      if (err) return done(err);
+      verifyAuth(opts.port, function (err) {
+        opts.action = 'stop';
+        run(opts, function(err) {
+          if (err) return done(err);
+          done();
+        });
+      });
+    });
+  });
+
+  it.only('should start a replicaset with auth', function(done) {
+    var opts = {
+      action: 'start',
+      name: 'mongodb-runner-test-replicaset-auth',
+      topology: 'replicaset',
+      port: 31000,
+      auth: true
+    };
+
+    run(opts, function(err) {
+      if (err) return done(err);
+      verifyAuth(opts.port, function (err) {
+        opts.action = 'stop';
+        run(opts, function(err) {
+          if (err) return done(err);
+          done();
+        });
+      });
+    });
+  });
+
+  it('should start a cluster with auth', function(done) {
+    var opts = {
+      action: 'start',
+      name: 'mongodb-runner-test-cluster-auth',
+      topology: 'cluster',
+      port: 32000,
+      auth: true
+    };
+
+    run(opts, function(err) {
+      if (err) return done(err);
+      verifyAuth(opts.port, function (err) {
+        opts.action = 'stop';
+        run(opts, function(err) {
+          if (err) return done(err);
+          done();
+        });
+      });
+    });
+  });
+
 });
+
+var verifyAuth = function (port, callback){
+  debug("Verifying Auth");
+  mongodb.MongoClient.connect(format('mongodb://localhost:%s/test', port), function(err, db) {
+    if (err) return callback(err);
+    db.collection('fruit').insertOne({'variety':'apple'}, function(err, r){
+      console.log("ERROR: ", err)
+      assert(err,'no error on insert');
+      callback(null);
+    })
+  });
+}
